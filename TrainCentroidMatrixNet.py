55f44fae7a6f2e3bf50bcea4a1b4a24f0c739367
Quang Ngoc <21020550@vnu.edu.vn>
2024-04-16 16:26:20 +0700

    optimize finetunning module 1

--- TrainCentroidMatrixNet.py
+++ TrainCentroidMatrixNet.py
@@ -16,14 +16,19 @@ class FineTunedNet(nn.Module):
         self.fc1 = nn.Linear(args.centord_Vector_Length, N_CLASSES)
         self.fc1.weight = nn.Parameter(centroid_matrix)
         self.fc1.bias = nn.Parameter(torch.zeros(N_CLASSES,))
+        # self.fc2 = nn.Linear(50, 30)
+        # self.fc3 = nn.Linear(30, N_CLASSES)
+        # self.fc3.weight = nn.Parameter(centroid_matrix)
+        # self.fc3.bias = nn.Parameter(torch.zeros(N_CLASSES, ))
         # self.fc2 = nn.Linear(250, 250)
         # self.fc3 = nn.Linear(250, 250)
         # self.fc4 = nn.Linear(250, args.centord_Vector_Length)
 
     def forward(self, x):
         x = F.softmax(self.fc1(x), dim=1)
+        # x = F.relu(self.fc1(x))
         # x = F.relu(self.fc2(x))
-        # x = F.relu(self.fc3(x))
-        # x = self.fc4(x)
+        # x = F.softmax(self.fc3(x), dim=1)
+
         return x
 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import time
+
 import torch
 import numpy as np
 
-from EmbeddingNet import EmbeddingNet
+from EmbeddingNet import EmbeddingNet, cosine_similarity
 from JointNetDrift import Joint_Prediction
 import torch.optim as optim
 
@@ -126,104 +128,6 @@ train_dataloader, test_dataloader, finetune_dataloader = init_dataloader()
 splitted_test_set = split_drift_type(test_dataloader)
 
 
-def main():
-
-    ModelSelect = args.FAN # 'RNN', 'FAN', 'FNN','FQN','CNN'
-    # train_dataloader,test_dataloader=init_dataloader()
-    # splitted_test_set = split_drift_type(test_dataloader)
-    print('Checking if GPU is available')
-    use_gpu = torch.cuda.is_available()
-    use_gpu = False
-
-    # Set training iterations and display period
-    num_episode = args.num_episode
-
-    # Initializing prototypical net
-    print('Initializing Joint_Prediction net')
-    def train():
-        model = Joint_Prediction(use_gpu=use_gpu, Data_Vector_Length =args.Data_Vector_Length, ModelSelect=ModelSelect)
-        # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.5)
-        optimizer = optim.Adam(model.parameters(), lr=args.lr)
-        lr_scheduler = init_lr_scheduler(optimizer)
-
-        train_loss = []
-        train_class_acc = []
-        train_loc_acc = []
-        test_loss = []
-        test_class_acc = []
-        test_loc_acc = []
-        centroid_matrix=torch.Tensor()
-        # Training loop
-        for i in range(num_episode):
-            model.train()
-            for batch_idx,data in enumerate(train_dataloader):
-                optimizer.zero_grad()
-                datax,datay,locy = data
-                # locy = torch.unsqueeze(locy, 1)
-                loss,class_acc,loc_acc,centroid_matrix = model(datax,datay,locy)
-                # loss = loc_loss_fun(pre_loc_y,locy)
-                loss.backward()
-                optimizer.step()
-                train_loss.append(loss.item())
-                train_class_acc.append(class_acc.item())
-                train_loc_acc.append(loc_acc.item())
-                centroid_matrix=centroid_matrix
-
-            avg_loss = np.mean(train_loss)
-            avg_class_acc = np.mean(train_class_acc)
-            avg_loc_acc = np.mean(train_loc_acc)
-            print('{} episode,Avg Train Loss: {}, Avg Train Class Acc: {}, Avg Train loc Acc: {}'.format(
-                i,avg_loss, avg_class_acc,avg_loc_acc))
-
-            lr_scheduler.step()
-
-        # Check whether the file exists
-        folder_path = "./input/Model/"+args.DATA_FILE
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-        PATH = './input/Model/'+args.DATA_FILE+'/{name}_model_embeding.pkl'.format(name=ModelSelect)
-        torch.save(model.state_dict(), PATH)
-        # save centroid matrix
-        torch.save(centroid_matrix, './input/Model/' + args.DATA_FILE + '/{name}_centroid_matrix.pt'.format(name=ModelSelect))
-        # save loss
-        with open('./input/Model/' + args.DATA_FILE + '/{name}_loss.json'.format(name=ModelSelect), "w") as f:
-            json.dump({"train_loss":np.array(train_loss[len(train_loss)-5:]).sum()/len(train_loss[len(train_loss)-5:]),
-                       "train_class_acc":np.array(train_class_acc[len(train_class_acc)-5:]).sum()/len(train_class_acc[len(train_class_acc)-5:]),
-                       "train_loc_acc":np.array(train_loc_acc[len(train_loc_acc)-5:]).sum()/len(train_loc_acc[len(train_loc_acc)-5:])}, f)
-
-    # train()
-    # Test loop
-    centroid_matrix = torch.load(
-        './input/Model/' + args.DATA_FILE + '/{name}_centroid_matrix.pt'.format(name=ModelSelect))
-    model = Teacher_Joint_Prediction(use_gpu=use_gpu, Data_Vector_Length=args.Data_Vector_Length,
-                                             ModelSelect=ModelSelect)
-    PATH = './input/Model/' + args.DATA_FILE + '/' + ModelSelect + '_model_embeding.pkl'
-    model.load_state_dict(torch.load(PATH))
-
-    CM = torch.zeros(N_CLASSES,N_CLASSES)
-
-
-    test_class_acc = []
-    for batch_idx, data in enumerate(test_dataloader):
-        datax,datay,_ = data
-        y_pred, _, _ = model(datax, './input/Model/' + args.DATA_FILE)
-        y_pred = torch.t(y_pred)
-        # loss = loss_fn(y_pred, datay)
-        # test_loss.append(loss.item())
-        pred_labels = torch.argmax(y_pred, dim=1)
-        class_acc = (pred_labels == datay).sum().item() / len(datay)
-        test_class_acc.append(class_acc)
-        CM = torch.add(CM, multiclass_confusion_matrix(pred_labels, datay, num_classes=N_CLASSES))
-    avg_class_acc = np.mean(test_class_acc)
-    # avg_loc_acc = np.mean(test_loc_acc)
-    # save result
-    print(f'Confusion Matrix Pretrain:\n {CM}')
-    with open('./input/Model/' + args.DATA_FILE + '/{name}_result.json'.format(name=ModelSelect), "w") as f:
-        json.dump({"avg_class_acc":avg_class_acc,"avg_loc_acc":""}, f)
-
-    print(' Avg Test Class Acc: {}, Avg Test loc Acc: {}'.format(
-        avg_class_acc, ""))
-
 def pretrain():
 
     ModelSelect = args.FAN # 'RNN', 'FAN', 'FNN','FQN','CNN'
@@ -284,7 +188,7 @@ def pretrain():
             json.dump({"train_loss":np.array(train_loss[len(train_loss)-5:]).sum()/len(train_loss[len(train_loss)-5:]),
                        "train_class_acc":np.array(train_class_acc[len(train_class_acc)-5:]).sum()/len(train_class_acc[len(train_class_acc)-5:])}, f)
 
-    train()
+    # train()
 
     # Test loop
     centroid_matrix = torch.load(
@@ -296,19 +200,20 @@ def pretrain():
 
     CM = torch.zeros(N_CLASSES,N_CLASSES)
 
-
     test_class_acc = []
+    # time1 = time.time()
     for batch_idx, data in enumerate(test_dataloader):
         datax,datay,_ = data
-        y_pred = model.forward_test(datax, centroid_matrix)
-        # y_pred = torch.t(y_pred)
-        # loss = loss_fn(y_pred, datay)
-        # test_loss.append(loss.item())
+        embedded_x = model.PrototypicalNet(datax)
+        y_pred = torch.t(cosine_similarity(centroid_matrix, embedded_x))
+        # y_pred = model.forward_test(datax, centroid_matrix)
         pred_labels = torch.argmax(y_pred, dim=1)
         class_acc = (pred_labels == datay).sum().item() / len(datay)
         test_class_acc.append(class_acc)
         CM = torch.add(CM, multiclass_confusion_matrix(pred_labels, datay, num_classes=N_CLASSES))
 
+    # time2 = time.time()
+    # print(time2 - time1)
     avg_class_acc = np.mean(test_class_acc)
     # avg_loc_acc = np.mean(test_loc_acc)
     # save result
@@ -344,9 +249,9 @@ def finetunning():
         print('Initializing Finetunning net')
         # sub_model = Sub_Joint_Prediction(Data_Vector_Length=args.Data_Vector_Length)
 
-        optimizer = optim.Adam(finetuning_model.parameters(), lr=0.02)
+        optimizer = optim.Adam(finetuning_model.parameters(), lr=0.05)
         lr_scheduler = init_lr_scheduler(optimizer)
-        loss_fn = torch.nn.CrossEntropyLoss()
+        loss_fn = torch.nn.CrossEntropyLoss(label_smoothing=0.1)
 
         train_loss = []
         train_class_acc = []
@@ -360,25 +265,18 @@ def finetunning():
             for batch_idx, data in enumerate(finetune_dataloader):
                 optimizer.zero_grad()
                 datax, datay, locy = data
-                # teacher model predict
                 embedded_x = embedding_model(datax)
                 y_pred = finetuning_model(embedded_x)
                 loss = loss_fn(y_pred, datay)
-
                 pred_labels = torch.argmax(y_pred, dim=1)
                 class_acc = (pred_labels == datay).sum().item()/len(datay)
                 train_class_acc.append(class_acc)
-
                 loss.backward()
                 optimizer.step()
                 train_loss.append(loss.item())
 
-                # train_class_acc.append(type_acc)
-                # train_loc_acc.append(loc_acc.item())
-
             avg_loss = np.mean(train_loss)
             avg_class_acc = np.mean(train_class_acc)
-            # avg_loc_acc = np.mean(train_loc_acc)
             print('{} episode,Avg Train Loss: {},Avg Train Class Acc: {}'.format(
                 i, avg_loss, avg_class_acc))
 
@@ -391,12 +289,14 @@ def finetunning():
                   "w") as f:
             json.dump({"train_loss": np.array(train_loss[-N_LAST_ELEMENTS:]).sum()/N_LAST_ELEMENTS, "train_class_acc": np.array(train_class_acc[-N_LAST_ELEMENTS:]).sum()/N_LAST_ELEMENTS}, f)
     # Test loop
-    train()
+    # train()
+
     finetuning_model = FineTunedNet(centroid_matrix=centroid_matrix)
     PATH = './input/Model/' + args.DATA_FILE + '/' + ModelSelect + '_finetune_model_embeding.pkl'
     finetuning_model.load_state_dict(torch.load(PATH))
     test_class_acc = []
     CM = torch.zeros(N_CLASSES, N_CLASSES)
+    # time1 = time.time()
     for batch_idx, data in enumerate(test_dataloader):
         datax, datay, locy = data
         # type_pred_T, loc_pred_T, loc_W = teather_model(datax, BASE_PATH)
@@ -409,6 +309,9 @@ def finetunning():
         class_acc = (pred_labels == datay).sum().item() / len(datay)
         test_class_acc.append(class_acc)
         CM = torch.add(CM, multiclass_confusion_matrix(pred_labels, datay, num_classes=N_CLASSES))
+
+    # time2 = time.time()
+    # print(time2 - time1)
     CM = CM.type(dtype=torch.int)
     print(f'\nConfusion Matrix FineTune:\n {CM}')
     avg_class_acc = np.mean(test_class_acc)
@@ -422,177 +325,10 @@ def finetunning():
         avg_class_acc))
 
 
-def knowledge_distillation():
-    '''
-    Knowledge Distillation
-    Distill a large model into a small one
-    '''
-    ModelSelect = args.FAN  # 'RNN', 'FAN','CNN', 'FNN','FQN','FAN'
-    train_dataloader, test_dataloader = init_dataloader()
-
-    print('Checking if GPU is available')
-    use_gpu = torch.cuda.is_available()
-    use_gpu = False
-
-    # Set training iterations and display period
-    num_episode = args.student_num_episode
-    #load teacher model
-    BASE_PATH = './input/Model/' + args.DATA_FILE
-    teather_model = Teacher_Joint_Prediction(use_gpu=use_gpu, Data_Vector_Length=args.Data_Vector_Length,
-                                ModelSelect=ModelSelect)
-    PATH = BASE_PATH +'/'+ ModelSelect +'_model_embeding.pkl'
-    teather_model.load_state_dict(torch.load(PATH))
-    centroid_matrix = torch.load(
-        './input/Model/' + args.DATA_FILE + '/{name}_centroid_matrix.pt'.format(name=ModelSelect))
-
-    finetuning_model = FineTunedNet(centroid_matrix=centroid_matrix)
-    PATH = BASE_PATH + '/{name}_finetune_model_embeding.pkl'.format(name=ModelSelect)
-    finetuning_model.load_state_dict(torch.load(PATH))
-    # Initializing prototypical net
-    print('Initializing Joint_Prediction net')
-    sub_model = Sub_Joint_Prediction(Data_Vector_Length=args.Data_Vector_Length)
-    optimizer = optim.Adam(sub_model.parameters(), lr=args.student_lr)
-    lr_scheduler = init_lr_scheduler(optimizer)
-
-    train_loss = []
-    train_class_acc = []
-    train_loc_acc = []
-    test_loss = []
-    test_class_acc = []
-    test_loc_acc = []
-    # Training loop
-    for i in range(num_episode):
-        sub_model.train()
-        for batch_idx, data in enumerate(train_dataloader):
-            optimizer.zero_grad()
-            datax, datay, locy = data
-            #teacher model predict
-            # type_pred_T,loc_pred_T,loc_W = teather_model(datax, BASE_PATH)
-            embedded_x = teather_model.PrototypicalNet(datax)
-            type_pred_T = finetuning_model(embedded_x)
-            _, loc_pred_T, loc_W = teather_model(datax, BASE_PATH)
-            loss,type_loss,loc_loss,loc_acc,type_acc = sub_model(datax, datay,locy,type_pred_T,loc_pred_T,loc_W)
-            loss.backward()
-            optimizer.step()
-            train_loss.append(loss.item())
-            train_class_acc.append(type_acc)
-            train_loc_acc.append(loc_acc.item())
-
-        avg_loss = np.mean(train_loss)
-        avg_class_acc = np.mean(train_class_acc)
-        avg_loc_acc = np.mean(train_loc_acc)
-        print('{} episode,Avg Train Loss: {},Avg Train Class Acc: {}, Avg Train loc Acc: {}'.format(
-            i, avg_loss, avg_class_acc, avg_loc_acc))
-
-        lr_scheduler.step()
-
-    PATH = './input/Model/' + args.DATA_FILE + '/{name}_student_{T}_{M}_model_embeding.pkl'.format(name=ModelSelect,
-                                                                                                      T=args.distillation_T,
-                                                                                                      M=args.distillation_point_method)
-    torch.save(sub_model.state_dict(), PATH)
-    # save loss
-    with open('./input/Model/' + args.DATA_FILE + '/{name}_student_{T}_{M}_loss.json'.format(name=ModelSelect,
-                                                                                                      T=args.distillation_T,
-                                                                                                      M=args.distillation_point_method), "w") as f:
-        json.dump({"train_loss": train_loss, "train_class_acc": train_class_acc, "train_loc_acc": train_loc_acc}, f)
-    # Test loop
-    for batch_idx, data in enumerate(test_dataloader):
-        datax, datay, locy = data
-        embedded_x = teather_model.PrototypicalNet(datax)
-        type_pred_T = finetuning_model(embedded_x)
-        _, loc_pred_T,loc_W = teather_model(datax, BASE_PATH)
-        loss,type_loss,loc_loss,loc_acc,type_acc = sub_model(datax, datay,locy,type_pred_T,loc_pred_T,loc_W)
-        test_loss.append(loss.item())
-        test_class_acc.append(type_acc)
-        test_loc_acc.append(loc_acc.item())
-    avg_class_acc = np.mean(test_class_acc)
-    avg_loc_acc = np.mean(test_loc_acc)
-    # save result
-    with open('./input/Model/' + args.DATA_FILE + '/{name}_student_{T}_{M}_result.json'.format(name=ModelSelect,
-                                                                                                      T=args.distillation_T,
-                                                                                                      M=args.distillation_point_method), "w") as f:
-        json.dump({"avg_class_acc": avg_class_acc, "avg_loc_acc": avg_loc_acc}, f)
-
-    print(' Avg Test Class Acc: {}, Avg Test loc Acc: {}'.format(
-        avg_class_acc, avg_loc_acc))
-
-def small_model():
-    '''
-    small model
-    '''
-    ModelSelect = args.FAN  # 'RNN', 'FAN','CNN', 'FNN','FQN','FAN'
-    train_dataloader, test_dataloader = init_dataloader()
-
-    print('Checking if GPU is available')
-    use_gpu = torch.cuda.is_available()
-    # use_gpu = False
-
-    # Set training iterations and display period
-    num_episode = args.num_episode
-
-    # Initializing prototypical net
-    print('Initializing Joint_Prediction net')
-    sub_model = Small_Prediction(Data_Vector_Length=args.Data_Vector_Length)
-    optimizer = optim.Adam(sub_model.parameters(), lr=args.lr)
-    lr_scheduler = init_lr_scheduler(optimizer)
-
-    train_loss = []
-    train_class_acc = []
-    train_loc_acc = []
-    test_loss = []
-    test_class_acc = []
-    test_loc_acc = []
-    # Training loop
-    for i in range(num_episode):
-        sub_model.train()
-        for batch_idx, data in enumerate(train_dataloader):
-            optimizer.zero_grad()
-            datax, datay, locy = data
-            #teacher model predict
-            loss,type_loss,loc_loss,loc_acc,type_acc = sub_model(datax, datay,locy)
-            loss.backward()
-            optimizer.step()
-            train_loss.append(loss.item())
-            train_class_acc.append(type_acc)
-            train_loc_acc.append(loc_acc.item())
-
-        avg_loss = np.mean(train_loss)
-        avg_class_acc = np.mean(train_class_acc)
-        avg_loc_acc = np.mean(train_loc_acc)
-        print('{} episode,Avg Train Loss: {}, Avg Train Class Acc: {}, Avg Train loc Acc: {}'.format(
-            i, avg_loss, avg_class_acc, avg_loc_acc))
-
-        lr_scheduler.step()
-    PATH = './input/Model/' + args.DATA_FILE + '/{name}_small_model_embeding.pkl'.format(name=ModelSelect)
-    torch.save(sub_model.state_dict(), PATH)
-    # save loss
-    with open('./input/Model/' + args.DATA_FILE + '/{name}_small_loss.json'.format(name=ModelSelect), "w") as f:
-        json.dump({"train_loss": train_loss, "train_class_acc": train_class_acc, "train_loc_acc": train_loc_acc}, f)
-    # Test loop
-    for batch_idx, data in enumerate(test_dataloader):
-        datax, datay, locy = data
-        loss,type_loss,loc_loss,loc_acc,type_acc = sub_model(datax, datay,locy)
-        test_loss.append(loss.item())
-        test_class_acc.append(type_acc)
-        test_loc_acc.append(loc_acc.item())
-    avg_class_acc = np.mean(test_class_acc)
-    avg_loc_acc = np.mean(test_loc_acc)
-    # save result
-    with open('./input/Model/' + args.DATA_FILE + '/{name}_small_result.json'.format(name=ModelSelect), "w") as f:
-        json.dump({"avg_class_acc": avg_class_acc, "avg_loc_acc": avg_loc_acc}, f)
-
-    print(' Avg Test Class Acc: {}, Avg Test loc Acc: {}'.format(
-        avg_class_acc, avg_loc_acc))
-
 
 if __name__ == "__main__":
-    # TODO Training teacher network
-    # main()
+    # TODO Pretrain
     pretrain()
     # TODO Finetune
     finetunning()
-    # TODO Distill the trained teacher network into the student network
-    # knowledge_distillation()
-    # # TODO Do not train the student network by distillation
-    # small_model()
 
